960293abb7a7e1941d0abeb50e71145608cad8f4
Evan <daley.evan@gmail.com>
2016-10-01 12:13:47 -0700

    Add Music and Create Light Cycle Map

---
+++ 2OCT2016/Assets/LightCycle/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	bool alive = true;
+	Vector3 startPos;
+
+	void Start () 
+	{
+		startPos = transform.position;
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void Die()
+	{
+		if (alive == true)
+		{
+			alive = false;
+			Invoke ("Respawn", .1f);
+		}
+	}
+
+	void Respawn()
+	{
+		transform.position = startPos;
+	}
+}
---
+++ 2OCT2016/Assets/UtilityScripts/ScoreKeeper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public static ScoreKeeper Instance;
+
+	private int score;
+	private int record;
+
+	void Start () 
+	{
+		record = PlayerPrefs.GetInt ("record", 0);
+	}
+
+	public void ResetScore()
+	{
+		score = 0;
+	}
+
+	public void AddScore(int points)
+	{
+		score += points;
+
+		if (score > record)
+		{
+			record = score;
+			PlayerPrefs.SetInt ("record", record);
+		}
+	}
+
+	public int Score
+	{
+		get { return score; }
+	}
+
+	public int Record
+	{
+		get { return score; }
+	}
+}
